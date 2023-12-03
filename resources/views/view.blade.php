55707a1a1a1d77dcb4bd93890fbdafb0b74e69ef
Natashayp <natashayosefani@gmail.com>
2023-12-04 01:30:38 +0700

    Tugas pertemuan tanggal 30 Nop

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -12,7 +12,13 @@
 	<br/>
 	<br/>
 
-	@foreach($pegawai as $p)
+    <div class="row">
+        <div class="col-4">
+
+        </div>
+        <div class="col-8">
+            @foreach($pegawai as $p)
+    <fieldset disabled>
 	<form action="/pegawai/update" method="post" class="form-horizontal">
 		{{ csrf_field() }}
         <div class="form-group row">
@@ -42,10 +48,13 @@
         </div>
 		<input type="hidden" name="id" value="{{ $p->pegawai_id }}"> <br/>
 
-		<input type="submit" value="Simpan Data" class="btn btn-primary">
+
 	</form>
+    </fieldset>
+    <a href="/pegawai"><input  type="submit" value="Oke" class="btn btn-primary"></a>
+    </div>
+
+</div>
 	@endforeach
 
     @endsection
-
-
